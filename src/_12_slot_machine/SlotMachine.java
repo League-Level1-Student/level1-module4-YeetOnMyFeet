04c1b36c904f9fab9b1bb8edf4da1f23ed4ca474
seema <seema@DESKTOP-9RUA99R>
2020-08-20 16:36:19 -0700

    finished level1 module4

--- src/_12_slot_machine/SlotMachine.java
+++ src/_12_slot_machine/SlotMachine.java
@@ -2,15 +2,19 @@ package _12_slot_machine;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Random;
 
-import javax.print.DocFlavor.URL;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class SlotMachine implements ActionListener{
@@ -19,14 +23,34 @@ public class SlotMachine implements ActionListener{
 	JLabel label = new JLabel();
 	JLabel label2 = new JLabel();
 	JLabel label3 = new JLabel();
+	JLabel label4 = new JLabel();
 	JButton button = new JButton();
-	Random ran = new Random();
-	int random = ran.nextInt(3);
-	
-	
-	
+	Random ran1 = new Random();
+	int r1;
+	Random ran2 = new Random();
+	int r2;
+	Random ran3 = new Random();
+	int r3;
+	int tries = 3;
+
+	public void run() {
+		frame.add(panel);
+		panel.add(button);
+		panel.add(label);
+		panel.add(label2);
+		panel.add(label3);
+		panel.add(label4);
+		label4.setText("Tries Remaining: " + tries);
+		frame.setVisible(true);
+		button.addActionListener(this);
+		button.setVisible(true);
+		button.setText("Spin");
+		frame.pack();
+		
+		
+	}
 	private JLabel createLabelImage(String fileName) throws MalformedURLException{
-        java.net.URL imageURL = getClass().getResource(fileName);
+        URL imageURL = getClass().getResource(fileName);
 	if (imageURL == null){
 		System.err.println("Could not find image " + fileName);
 		return new JLabel();
@@ -35,31 +59,127 @@ public class SlotMachine implements ActionListener{
 	JLabel imageLabel = new JLabel(icon);
 	return imageLabel;
 }
-public void run() {
-	frame.add(panel);
-	panel.add(button);
-	panel.add(label);
-	panel.add(label2);
-	panel.add(label3);
-	frame.setVisible(true);
-	button.setVisible(true);
-	button.setText("SPIN");
-	frame.pack();
-	
-	
-}
-
-
-
-
+    
 
 @Override
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
-	if (e.getSource() == button) {
-		
+	panel.remove(label);
+	r1 = ran1.nextInt(3);
+	System.out.println(r1);
+	panel.remove(label2);
+	r2 = ran2.nextInt(3);
+	System.out.println(r2);
+	panel.remove(label3);
+	r3 = ran3.nextInt(3);
+	System.out.println(r3);
+	
+	if (r1 == 0) {
+		try {
+			label = createLabelImage("neworange.jpg");
+		} catch (MalformedURLException e1) {
+			// TODO: handle exception
+			e1.printStackTrace();
+		}
+		panel.add(label);
+	}
+	else if (r1 == 1) {
+		try {
+			label = createLabelImage("cherry.jpg");
+		} catch (MalformedURLException e1) {
+			// TODO: handle exception
+			e1.printStackTrace();
+		}
+		panel.add(label);
+	}
+	else if (r1 ==2) {
+		try {
+			label = createLabelImage("lime.jpg");
+		} catch (MalformedURLException e1) {
+			// TODO: handle exception
+			e1.printStackTrace();
+		}
+		panel.add(label);
+	}
+	if (r2 == 0) {
+		try {
+			label2 = createLabelImage("neworange.jpg");
+		} catch (MalformedURLException e1) {
+			// TODO: handle exception
+			e1.printStackTrace();
+		}
+		panel.add(label2);
+	}
+	else if (r2 == 1) {
+		try {
+			label2 = createLabelImage("cherry.jpg");
+		} catch (MalformedURLException e1) {
+			// TODO: handle exception
+			e1.printStackTrace();
+		}
+		panel.add(label2);
+	}
+	else if (r2 ==2) {
+		try {
+			label2 = createLabelImage("lime.jpg");
+		} catch (MalformedURLException e1) {
+			// TODO: handle exception
+			e1.printStackTrace();
+		}
+		panel.add(label2);
+	}
+	if (r3 == 0) {
+		try {
+			label3 = createLabelImage("neworange.jpg");
+		} catch (MalformedURLException e1) {
+			// TODO: handle exception
+			e1.printStackTrace();
+		}
+		panel.add(label3);
+	}
+	else if (r3 == 1) {
+		try {
+			label3 = createLabelImage("cherry.jpg");
+		} catch (MalformedURLException e1) {
+			// TODO: handle exception
+			e1.printStackTrace();
+		}
+		panel.add(label3);
+	}
+	else if (r3 ==2) {
+		try {
+			label3 = createLabelImage("lime.jpg");
+		} catch (MalformedURLException e1) {
+			// TODO: handle exception
+			e1.printStackTrace();
+		}
+		panel.add(label3);
+	}
+	frame.add(panel);
+	frame.pack();
+	if (r1 == r2 && r2 == r3) {
+		JOptionPane.showMessageDialog(null, "You win!");
+		tries = 3;
+	}
+	
+	else if (tries == 0 ) {
+		JOptionPane.showMessageDialog(null, "You lose!");
+		tries = 3;
+	}
+	
+	else if (e.getSource() == button) {
+		tries = tries - 1;
+	}
+	label4.setText("Tries Remaining: " + tries);
 	}
 }
+
+
+
+
+
+
+
     
 
-}
+
